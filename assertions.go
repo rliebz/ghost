5f46c78fb4b8a580d6070e69ba8908c1148113ba
Robert Liebowitz <rliebz@gmail.com>
2023-02-28 01:29:39 -0500

    Add more assertions

--- assertions.go
+++ assertions.go
@@ -8,12 +8,47 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+// BeNil asserts that the given value is nil.
+func BeNil(v any) Assertion {
+	return func() Result {
+		if v == nil {
+			return Result{
+				Success: true,
+				Message: "value is nil",
+			}
+		}
+
+		return Result{
+			Success: false,
+			Message: fmt.Sprintf("%q is not nil", v),
+		}
+	}
+}
+
 // BeTrue asserts that a value is true.
 func BeTrue(b bool) Assertion {
 	return func() Result {
 		return Result{
 			Success: b,
 			Message: fmt.Sprintf("value was %t", b),
+		}
+	}
+}
+
+// BeZero asserts that the given value equals its zero value.
+func BeZero[T comparable](v T) Assertion {
+	return func() Result {
+		var zero T
+		if v == zero {
+			return Result{
+				Success: true,
+				Message: "%v is the zero value",
+			}
+		}
+
+		return Result{
+			Success: false,
+			Message: fmt.Sprintf("%v is non-zero", v),
 		}
 	}
 }
@@ -41,13 +76,13 @@ func ContainString(s, substr string) Assertion {
 		if strings.Contains(s, substr) {
 			return Result{
 				Success: true,
-				Message: fmt.Sprintf("%v present in %v", substr, s),
+				Message: fmt.Sprintf("%q present in %q", substr, s),
 			}
 		}
 
 		return Result{
 			Success: false,
-			Message: fmt.Sprintf("%v not present in %v", substr, s),
+			Message: fmt.Sprintf("%q not present in %q", substr, s),
 		}
 	}
 }
@@ -86,8 +121,8 @@ func Equal[T comparable](want, got T) Assertion {
 	}
 }
 
-// Err asserts that an error is non-nil.
-func Err(err error) Assertion {
+// Error asserts that an error is non-nil.
+func Error(err error) Assertion {
 	return func() Result {
 		if err == nil {
 			return Result{
@@ -99,6 +134,30 @@ func Err(err error) Assertion {
 		return Result{
 			Success: true,
 			Message: fmt.Sprintf("error found with value: %q", err),
+		}
+	}
+}
+
+// ErrorContaining asserts that a string contains a particular substring.
+func ErrorContaining(err error, msg string) Assertion {
+	return func() Result {
+		if err == nil {
+			return Result{
+				Success: false,
+				Message: "error was nil",
+			}
+		}
+
+		if strings.Contains(err.Error(), msg) {
+			return Result{
+				Success: true,
+				Message: fmt.Sprintf("%q present in error %q", msg, err),
+			}
+		}
+
+		return Result{
+			Success: false,
+			Message: fmt.Sprintf("%q not present in error %q", msg, err),
 		}
 	}
 }
