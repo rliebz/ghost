abc22048890e2b148019dc6e35d3264fe52edcf1
Robert Liebowitz <rliebz@gmail.com>
2023-03-05 09:09:57 -0500

    Use AST parsing to improve error messages

--- assertions.go
+++ assertions.go
@@ -8,138 +8,187 @@ import (
 
 // BeNil asserts that the given value is nil.
 func BeNil(v any) Assertion {
+	fv := fmt.Sprintf("%v", v)
+	if args, ok := getFormattedArgs(1); ok {
+		fv = args[0]
+	}
+
 	return func() Result {
 		if v == nil {
 			return Result{
 				Success: true,
-				Message: "value is nil",
+				Message: fmt.Sprintf("%v is nil", fv),
 			}
 		}
 
 		return Result{
 			Success: false,
-			Message: fmt.Sprintf("%q is not nil", v),
+			Message: fmt.Sprintf("%v is %q, not nil", fv, v),
 		}
 	}
 }
 
 // BeTrue asserts that a value is true.
 func BeTrue(b bool) Assertion {
+	fb := fmt.Sprintf("%v", b)
+	if args, ok := getFormattedArgs(1); ok {
+		fb = args[0]
+	}
+
 	return func() Result {
 		return Result{
 			Success: b,
-			Message: fmt.Sprintf("value was %t", b),
+			Message: fmt.Sprintf("%v was %t", fb, b),
 		}
 	}
 }
 
 // BeZero asserts that the given value equals its zero value.
 func BeZero[T comparable](v T) Assertion {
+	fv := fmt.Sprintf("%v", v)
+	if args, ok := getFormattedArgs(1); ok {
+		fv = args[0]
+	}
+
 	return func() Result {
 		var zero T
 		if v == zero {
 			return Result{
 				Success: true,
-				Message: "%v is the zero value",
+				Message: fmt.Sprintf("%v is the zero value", fv),
 			}
 		}
 
 		return Result{
 			Success: false,
-			Message: fmt.Sprintf("%v is non-zero", v),
+			Message: fmt.Sprintf("%v is non-zero", fv),
 		}
 	}
 }
 
 // Contain asserts that a slice contains a particular element.
 func Contain[T comparable](slice []T, element T) Assertion {
+	fslice, felement := fmt.Sprintf("%v", slice), fmt.Sprintf("%v", element)
+	if args, ok := getFormattedArgs(1); ok {
+		fslice, felement = args[0], args[1]
+	}
+
+	// TODO: Print the values of the slices / elements
 	return func() Result {
 		for _, x := range slice {
 			if x == element {
 				return Result{
 					Success: true,
-					Message: fmt.Sprintf("%v present in %v", element, slice),
+					Message: fmt.Sprintf("%v contains %v", fslice, felement),
 				}
 			}
 		}
 
 		return Result{
 			Success: false,
-			Message: fmt.Sprintf("%v not present in %v", element, slice),
+			Message: fmt.Sprintf("%v does not contain %v", fslice, felement),
 		}
 	}
 }
 
 // ContainString asserts that a string contains a particular substring.
-func ContainString(s, substr string) Assertion {
+func ContainString(str, substr string) Assertion {
+	fstr, fsubstr := fmt.Sprintf("%v", str), fmt.Sprintf("%v", substr)
+	if args, ok := getFormattedArgs(1); ok {
+		fstr, fsubstr = args[0], args[1]
+	}
+
+	// TODO: Print the values
 	return func() Result {
-		if strings.Contains(s, substr) {
+		if strings.Contains(str, substr) {
 			return Result{
 				Success: true,
-				Message: fmt.Sprintf("%q present in %q", substr, s),
+				Message: fmt.Sprintf("%v contains %v", fstr, fsubstr),
 			}
 		}
 
 		return Result{
 			Success: false,
-			Message: fmt.Sprintf("%q not present in %q", substr, s),
+			Message: fmt.Sprintf("%q does not contain %q", fstr, fsubstr),
 		}
 	}
 }
 
 // DeepEqual asserts that two elements are deeply equal.
 func DeepEqual[T any](want, got T) Assertion {
+	fwant, fgot := fmt.Sprintf("%v", want), fmt.Sprintf("%v", got)
+	if args, ok := getFormattedArgs(1); ok {
+		fwant, fgot = args[0], args[1]
+	}
+
+	// TODO: Print the values
 	return func() Result {
 		if reflect.DeepEqual(want, got) {
 			return Result{
 				Success: true,
-				Message: fmt.Sprintf("%v == %v", want, got),
+				Message: fmt.Sprintf("%v == %v", fwant, fgot),
 			}
 		}
 
 		return Result{
 			Success: false,
-			Message: fmt.Sprintf("%v != %v", want, got),
+			Message: fmt.Sprintf("%v != %v", fwant, fgot),
 		}
 	}
 }
 
 // Equal asserts that two elements are equal.
-func Equal[T comparable](want, got T) Assertion {
+func Equal[T comparable](want T, got T) Assertion {
+	fwant, fgot := fmt.Sprintf("%v", want), fmt.Sprintf("%v", got)
+	if args, ok := getFormattedArgs(1); ok {
+		fwant, fgot = args[0], args[1]
+	}
+
+	// TODO: Print the values
 	return func() Result {
 		if want == got {
 			return Result{
 				Success: true,
-				Message: fmt.Sprintf("%v == %v", want, got),
+				Message: fmt.Sprintf("%v == %v", fwant, fgot),
 			}
 		}
 
 		return Result{
 			Success: false,
-			Message: fmt.Sprintf("%v != %v", want, got),
+			Message: fmt.Sprintf("%v != %v", fwant, fgot),
 		}
 	}
 }
 
 // Error asserts that an error is non-nil.
 func Error(err error) Assertion {
+	ferr := fmt.Sprintf("%v", err)
+	if args, ok := getFormattedArgs(1); ok {
+		ferr = args[0]
+	}
+
 	return func() Result {
 		if err == nil {
 			return Result{
 				Success: false,
-				Message: "error was nil",
+				Message: fmt.Sprintf("%s was nil", ferr),
 			}
 		}
 
 		return Result{
 			Success: true,
-			Message: fmt.Sprintf("error found with value: %q", err),
+			Message: fmt.Sprintf("%s contains error: %s", ferr, err),
 		}
 	}
 }
 
 // ErrorContaining asserts that a string contains a particular substring.
 func ErrorContaining(err error, msg string) Assertion {
+	ferr := fmt.Sprintf("%v", err)
+	if args, ok := getFormattedArgs(1); ok {
+		ferr = args[0]
+	}
+
 	return func() Result {
 		if err == nil {
 			return Result{
@@ -151,13 +200,13 @@ func ErrorContaining(err error, msg string) Assertion {
 		if strings.Contains(err.Error(), msg) {
 			return Result{
 				Success: true,
-				Message: fmt.Sprintf("%q present in error %q", msg, err),
+				Message: fmt.Sprintf("%v contains message %q: %v", ferr, msg, err),
 			}
 		}
 
 		return Result{
 			Success: false,
-			Message: fmt.Sprintf("%q not present in error %q", msg, err),
+			Message: fmt.Sprintf("%v does not contain message %q: %v", ferr, msg, err),
 		}
 	}
 }
---
+++ ast.go
@@ -0,0 +1,97 @@
+package ghost
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"runtime"
+	"strings"
+)
+
+func getFormattedArgs(skip int) ([]string, bool) {
+	args, err := callExprArgs(skip + 1)
+	if err != nil {
+		return nil, false
+	}
+
+	out := make([]string, 0, len(args))
+	for _, arg := range args {
+		out = append(out, nodeToString(arg))
+	}
+
+	return out, true
+}
+
+func callExprArgs(skip int) ([]ast.Expr, error) {
+	pc, _, _, _ := runtime.Caller(skip)
+	_, filename, line, ok := runtime.Caller(skip + 1)
+	if !ok {
+		return nil, errors.New("failed to get file/line")
+	}
+
+	fn := runtime.FuncForPC(pc)
+
+	fileset := token.NewFileSet()
+	astFile, err := parser.ParseFile(fileset, filename, nil, parser.AllErrors)
+	if err != nil {
+		return nil, err
+	}
+
+	node := callExprForFunc(fn, fileset, astFile, line)
+	if node == nil {
+		return nil, errors.New("no node found at line")
+	}
+
+	return node.Args, nil
+}
+
+func callExprForFunc(
+	want *runtime.Func,
+	fileset *token.FileSet,
+	node ast.Node,
+	lineNum int,
+) *ast.CallExpr {
+	var out *ast.CallExpr
+	ast.Inspect(node, func(node ast.Node) bool {
+		if node == nil {
+			return false
+		}
+
+		if fileset.Position(node.Pos()).Line != lineNum {
+			return true
+		}
+
+		if callExpr, ok := node.(*ast.CallExpr); ok && describesCallExpr(want, callExpr) {
+			out = callExpr
+		}
+
+		return true
+	})
+	return out
+}
+
+// TODO: There are likely more reliable ways to compare functions.
+func describesCallExpr(want *runtime.Func, callExpr *ast.CallExpr) bool {
+	switch fun := callExpr.Fun.(type) {
+	case *ast.Ident:
+		return strings.Contains(want.Name(), fun.Name)
+	case *ast.SelectorExpr:
+		return strings.Contains(want.Name(), nodeToString(fun))
+	case *ast.FuncLit:
+		return false
+	}
+
+	panic(fmt.Sprintf("unexpected *ast.CallExpr.Fun type: %T", callExpr.Fun))
+}
+
+func nodeToString(node ast.Node) string {
+	var buf bytes.Buffer
+	if err := format.Node(&buf, token.NewFileSet(), node); err != nil {
+		panic(err)
+	}
+	return buf.String()
+}
