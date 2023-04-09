bb2304f9dd54c39553936be0dff2c0cf492a992a
Robert Liebowitz <rliebz@gmail.com>
2023-04-09 07:52:56 -0400

    Clean up NoError tests

--- ghost_test.go
+++ ghost_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rliebz/ghost"
 )
 
-func TestRunner_NoErr(t *testing.T) {
+func TestRunner_NoError(t *testing.T) {
 	g := ghost.New(t)
 
 	mockT := newMockT()
@@ -17,15 +17,15 @@ func TestRunner_NoErr(t *testing.T) {
 	myErr := errors.New("oh no")
 	testG.NoError(myErr)
 
-	if g.Should(ghost.Equal(1, len(mockT.logCalls))) {
+	if g.Should(ghost.Len(1, mockT.logCalls)) {
 		g.Should(ghost.DeepEqual(
 			[]any{"myErr has error value: oh no"},
 			mockT.logCalls[0],
 		))
 	}
 
-	g.Should(ghost.Equal(0, len(mockT.failCalls)))
-	g.Should(ghost.Equal(1, len(mockT.failNowCalls)))
+	g.Should(ghost.Len(0, mockT.failCalls))
+	g.Should(ghost.Len(1, mockT.failNowCalls))
 }
 
 type mockT struct {
@@ -43,13 +43,22 @@ func newMockT() *mockT {
 }
 
 func (t *mockT) Log(args ...any) {
+	t.m.Lock()
+	defer t.m.Unlock()
+
 	t.logCalls = append(t.logCalls, args)
 }
 
 func (t *mockT) Fail() {
+	t.m.Lock()
+	defer t.m.Unlock()
+
 	t.failCalls = append(t.failCalls, struct{}{})
 }
 
 func (t *mockT) FailNow() {
+	t.m.Lock()
+	defer t.m.Unlock()
+
 	t.failNowCalls = append(t.failNowCalls, struct{}{})
 }
