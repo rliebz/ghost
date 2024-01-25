1bfa594943d6b7dc08d6151df30e5cdfb81166e7
Rob Liebowitz <rliebowitz@morningconsult.com>
2024-01-25 09:25:20 -0500

    Temporarily panic on AST parsing failures
    The refactor here is a good refactor, which should probably be merged
    into main. The panic is a bad idea, except for me debugging as we speak.

--- ghostlib/ast.go
+++ ghostlib/ast.go
@@ -16,23 +16,34 @@ import (
 // the AST. To handle situations where this cannot be done reliably, the raw
 // arguments should be passed so their values can be used as a backup.
 func ArgsFromAST(unformatted ...any) []string {
-	return argsFromASTSkip(1, unformatted...)
+	args, err := argsFromASTSkip(1)
+	if err != nil {
+		_ = mapString(unformatted) // return
+		panic(fmt.Sprintf("failed to parse args from the AST: %s", err))
+	}
+
+	return args
 }
 
 // argsFromASTSkip gets the string representation of the caller's arguments
 // from the AST, skipping the number specified.
-func argsFromASTSkip(skip int, unformatted ...any) []string {
+func argsFromASTSkip(skip int) ([]string, error) {
 	args, err := callExprArgs(2 + skip)
 	if err != nil {
-		return mapString(unformatted)
+		return nil, fmt.Errorf("getting call expr args: %w", err)
 	}
 
 	out := make([]string, 0, len(args))
-	for _, arg := range args {
-		out = append(out, nodeToString(arg))
+	for i, arg := range args {
+		s, err := nodeToString(arg)
+		if err != nil {
+			return nil, fmt.Errorf("converting node %d to string: %w", i, err)
+		}
+
+		out = append(out, s)
 	}
 
-	return out
+	return out, nil
 }
 
 func mapString(s []any) []string {
@@ -46,12 +57,12 @@ func mapString(s []any) []string {
 func callExprArgs(skip int) ([]ast.Expr, error) {
 	pc, _, _, ok := runtime.Caller(skip)
 	if !ok {
-		return nil, errors.New("failed to get file/line")
+		return nil, errors.New("failed to get file/line for skip")
 	}
 
 	_, filename, line, ok := runtime.Caller(skip + 1)
 	if !ok {
-		return nil, errors.New("failed to get file/line")
+		return nil, errors.New("failed to get file/line for skip + 1")
 	}
 
 	wantFunc := runtime.FuncForPC(pc)
@@ -59,7 +70,7 @@ func callExprArgs(skip int) ([]ast.Expr, error) {
 	fset := token.NewFileSet()
 	astFile, err := parser.ParseFile(fset, filename, nil, parser.AllErrors)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("parsing file: %w", err)
 	}
 
 	node := callExprForFunc(wantFunc, fset, astFile, line)
@@ -113,10 +124,10 @@ func describesCallExpr(wantFn *runtime.Func, callExpr *ast.CallExpr) bool {
 	return false
 }
 
-func nodeToString(node ast.Node) string {
+func nodeToString(node ast.Node) (string, error) {
 	var buf bytes.Buffer
 	if err := format.Node(&buf, token.NewFileSet(), node); err != nil {
-		panic(err)
+		return "", fmt.Errorf("formatting node: %w", err)
 	}
-	return buf.String()
+	return buf.String(), nil
 }
