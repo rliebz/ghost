590a260f527522415143c98bc623e0262c29344d
Robert Liebowitz <rliebz@gmail.com>
2025-01-01 07:38:50 -0500

    Add custom jsondiff implementation
    The main thing this lacks that the old library had is color support,
    which actually makes us a little more internally consistent for the time
    being. But it has the benefit of dropping a dependency, and slightly
    improving the visual clarity of JSON diffs in terminals that don't
    support color (as might be common when piping).
    
    This does not yet wire it in. Next commit will do that.

---
+++ internal/jsondiff/jsondiff.go
@@ -0,0 +1,357 @@
+// Package jsondiff prints a human-readable JSON diff.
+package jsondiff
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+// Kind describes the result of the diff categorically.
+type Kind int
+
+// These must be kept in order from least severe to most severe.
+const (
+	Match Kind = iota
+	NoMatch
+	GotInvalid
+	WantInvalid
+	BothInvalid
+)
+
+func (k Kind) String() string {
+	switch k {
+	case Match:
+		return "Match"
+	case NoMatch:
+		return "NoMatch"
+	case GotInvalid:
+		return "GotInvalid"
+	case WantInvalid:
+		return "WantInvalid"
+	case BothInvalid:
+		return "BothInvalid"
+	default:
+		return "InvalidKind"
+	}
+}
+
+// Diff returns a pretty JSON diff of two inputs.
+func Diff[T ~string | ~[]byte](got, want T) (string, Kind) {
+	gotValue, gotErr := decode(got)
+	wantValue, wantErr := decode(want)
+
+	switch {
+	case gotErr != nil && wantErr != nil:
+		return "", BothInvalid
+	case gotErr != nil:
+		return "", GotInvalid
+	case wantErr != nil:
+		return "", WantInvalid
+	}
+
+	d := newDiffer()
+	d.diffValues(gotValue, wantValue)
+	return d.buf.String(), d.kind
+}
+
+func decode[T ~string | ~[]byte](v T) (any, error) {
+	dec := json.NewDecoder(bytes.NewReader([]byte(v)))
+	dec.UseNumber()
+
+	var data any
+	err := dec.Decode(&data)
+	return data, err
+}
+
+type differ struct {
+	buf    *bytes.Buffer
+	kind   Kind
+	level  int
+	prefix byte
+}
+
+func newDiffer() *differ {
+	return &differ{
+		buf:    new(bytes.Buffer),
+		level:  1, // start non-zero to make space for +/-/~
+		prefix: ' ',
+	}
+}
+
+func (d *differ) diffValues(got, want any) {
+	switch {
+	case got == nil && want == nil:
+		d.writeValue(got)
+		return
+	case got == nil || want == nil:
+		d.writeMismatch(got, want)
+		return
+	}
+
+	gotType := reflect.TypeOf(got)
+	wantType := reflect.TypeOf(want)
+
+	// check types before converting to [reflect.Kind] to catch string/json.Number
+	if gotType != wantType {
+		d.writeMismatch(got, want)
+		return
+	}
+
+	switch gotType.Kind() {
+	case reflect.Bool:
+		if got != want {
+			d.writeMismatch(got, want)
+			return
+		}
+	case reflect.String:
+		if got != want {
+			d.writeMismatch(got, want)
+			return
+		}
+	case reflect.Map:
+		d.diffMaps(got.(map[string]any), want.(map[string]any))
+		return
+	case reflect.Slice:
+		d.diffSlices(got.([]any), want.([]any))
+		return
+	}
+
+	d.writeValue(got)
+}
+
+func (d *differ) diffMaps(got, want map[string]any) {
+	if len(got) == 0 && len(want) == 0 {
+		d.buf.WriteString("{}")
+		return
+	}
+
+	if d.buf.Len() == 0 {
+		d.writeIndent()
+	}
+
+	d.buf.WriteString("{")
+	d.level++
+
+	keys := make(map[string]struct{})
+	for k := range got {
+		keys[k] = struct{}{}
+	}
+	for k := range want {
+		keys[k] = struct{}{}
+	}
+
+	sortedKeys := make([]string, 0, len(keys))
+	for k := range keys {
+		sortedKeys = append(sortedKeys, k)
+	}
+	slices.Sort(sortedKeys)
+
+	first := true
+	for _, k := range sortedKeys {
+		gotValue, gotOk := got[k]
+		wantValue, wantOk := want[k]
+
+		switch {
+		case gotOk && wantOk:
+			d.writeElementSeparator(first)
+			fmt.Fprintf(d.buf, "%q: ", k)
+			d.diffValues(gotValue, wantValue)
+		case gotOk:
+			d.failMatch()
+			d.prefix = '+'
+			d.writeElementSeparator(first)
+			fmt.Fprintf(d.buf, "%q: ", k)
+			d.writeValue(gotValue)
+			d.prefix = ' '
+		case wantOk:
+			d.failMatch()
+			d.prefix = '-'
+			d.writeElementSeparator(first)
+			fmt.Fprintf(d.buf, "%q: ", k)
+			d.writeValue(wantValue)
+			d.prefix = ' '
+		default:
+			panic(fmt.Sprintf("unexpected key %q in map; this is a ghost error", k))
+		}
+
+		first = false
+	}
+	d.level--
+	d.writeNewlineIndent()
+	d.buf.WriteByte('}')
+}
+
+func (d *differ) diffSlices(got, want []any) {
+	if len(got) == 0 && len(want) == 0 {
+		d.buf.WriteString("[]")
+		return
+	}
+
+	if d.buf.Len() == 0 {
+		d.writeIndent()
+	}
+
+	d.buf.WriteByte('[')
+	d.level++
+	for i := 0; i < len(got) || i < len(want); i++ {
+		switch {
+		case i >= len(got):
+			d.failMatch()
+			d.prefix = '-'
+			d.writeExtraSliceElements(&i, want)
+			d.prefix = ' '
+		case i >= len(want):
+			d.failMatch()
+			d.prefix = '+'
+			d.writeExtraSliceElements(&i, got)
+			d.prefix = ' '
+		default:
+			d.writeElementSeparator(i == 0)
+			d.diffValues(got[i], want[i])
+		}
+	}
+	d.level--
+	d.writeNewlineIndent()
+	d.buf.WriteByte(']')
+}
+
+func (d *differ) writeExtraSliceElements(idx *int, s []any) {
+	for ; *idx < len(s); *idx++ {
+		d.writeElementSeparator(*idx == 0)
+		d.writeValue(s[*idx])
+	}
+}
+
+func (d *differ) writeValue(v any) {
+	switch v := v.(type) {
+	case bool:
+		d.buf.WriteString(strconv.FormatBool(v))
+	case json.Number:
+		d.buf.WriteString(string(v))
+	case string:
+		d.buf.WriteString(strconv.Quote(v))
+	case []any:
+		d.writeSlice(v)
+	case map[string]any:
+		d.writeMap(v)
+	default:
+		if v != nil {
+			panic(fmt.Sprintf("unexpected JSON type %T: %s; this is a ghost error", v, v))
+		}
+		d.buf.WriteString("null")
+	}
+}
+
+// writeValueInline is like [writeValue], but with abbrevations for long
+// strings, slices, and maps.
+func (d *differ) writeValueInline(v any) {
+	switch v := v.(type) {
+	case []any:
+		if len(v) == 0 {
+			d.buf.WriteString("[]")
+		} else {
+			d.buf.WriteString("[...]")
+		}
+	case map[string]any:
+		if len(v) == 0 {
+			d.buf.WriteString("{}")
+		} else {
+			d.buf.WriteString("{...}")
+		}
+	default:
+		d.writeValue(v)
+	}
+}
+
+func (d *differ) writeSlice(s []any) {
+	if len(s) == 0 {
+		d.buf.WriteString("[]")
+		return
+	}
+	d.buf.WriteByte('[')
+	d.level++
+	d.writeNewlineIndent()
+	for i, vv := range s {
+		if i != 0 {
+			d.buf.WriteByte(',')
+			d.writeNewlineIndent()
+		}
+		d.writeValue(vv)
+	}
+	d.level--
+	d.writeNewlineIndent()
+	d.buf.WriteByte(']')
+}
+
+func (d *differ) writeMap(m map[string]any) {
+	if len(m) == 0 {
+		d.buf.WriteString("{}")
+		return
+	}
+	d.buf.WriteByte('{')
+	d.level++
+
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	slices.Sort(keys)
+
+	first := true
+	for _, k := range keys {
+		v := m[k]
+
+		d.writeElementSeparator(first)
+
+		fmt.Fprintf(d.buf, "%q: ", k)
+		d.writeValue(v)
+
+		first = false
+	}
+	d.level--
+	d.writeNewlineIndent()
+	d.buf.WriteByte('}')
+}
+
+func (d *differ) writeMismatch(got, want any) {
+	// If we're in a multi-line diff, make sure it starts with [~]
+	data := d.buf.Bytes()
+	if idx := bytes.LastIndex(data, []byte("\n ")); idx != -1 {
+		data[idx+1] = '~'
+		d.buf.Reset()
+		d.buf.Write(data)
+	}
+
+	d.writeValueInline(want)
+	d.buf.WriteString(" => ")
+	d.writeValueInline(got)
+	d.failMatch()
+}
+
+// TODO: I would rather have the last element of EACH collection handle the
+// middle commas correctly. Probably a hefty refactor, can save it for later.
+func (d *differ) writeElementSeparator(first bool) {
+	if !first {
+		d.buf.WriteByte(',')
+	}
+	d.writeNewlineIndent()
+}
+
+func (d *differ) writeNewlineIndent() {
+	d.buf.WriteByte('\n')
+	d.writeIndent()
+}
+
+func (d *differ) writeIndent() {
+	d.buf.WriteByte(d.prefix)
+	d.buf.WriteString(strings.Repeat(" ", d.level*2-1))
+}
+
+func (d *differ) failMatch() {
+	d.kind = NoMatch
+}
---
+++ internal/jsondiff/jsondiff_test.go
@@ -0,0 +1,415 @@
+package jsondiff_test
+
+import (
+	"testing"
+
+	"github.com/rliebz/ghost"
+	"github.com/rliebz/ghost/be"
+	"github.com/rliebz/ghost/internal/jsondiff"
+)
+
+func TestDiff(t *testing.T) {
+	tests := []struct {
+		name     string
+		a        string
+		b        string
+		wantDiff string
+		wantKind jsondiff.Kind
+	}{
+		// Primitives
+		{
+			name:     "nulls equal",
+			a:        `null`,
+			b:        `null`,
+			wantDiff: `null`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name:     "bools equal",
+			a:        `true`,
+			b:        `true`,
+			wantDiff: `true`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name:     "bools not equal",
+			a:        `true`,
+			b:        `false`,
+			wantDiff: `false => true`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name:     "string equal",
+			a:        `"foo"`,
+			b:        `"foo"`,
+			wantDiff: `"foo"`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name:     "string not equal",
+			a:        `"foo"`,
+			b:        `"bar"`,
+			wantDiff: `"bar" => "foo"`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name:     "string and null",
+			a:        `"null"`,
+			b:        `null`,
+			wantDiff: `null => "null"`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name:     "number equal",
+			a:        `17.5`,
+			b:        `17.5`,
+			wantDiff: `17.5`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name:     "number not equal",
+			a:        `17.5`,
+			b:        `17.6`,
+			wantDiff: `17.6 => 17.5`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name:     "number and string",
+			a:        `17.5`,
+			b:        `"17.5"`,
+			wantDiff: `"17.5" => 17.5`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name:     "got invalid",
+			a:        `NaN`,
+			b:        `true`,
+			wantKind: jsondiff.GotInvalid,
+		},
+		{
+			name:     "want invalid",
+			a:        `true`,
+			b:        `NaN`,
+			wantKind: jsondiff.WantInvalid,
+		},
+		{
+			name:     "both invalid",
+			a:        `NaN`,
+			b:        `NaN`,
+			wantKind: jsondiff.BothInvalid,
+		},
+
+		// Objects
+		{
+			name:     "empty objects equal",
+			a:        `{}`,
+			b:        `{}`,
+			wantDiff: `{}`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name: "small objects equal",
+			a:    `{"a": 1}`,
+			b:    `{"a": 1}`,
+			wantDiff: `  {
+    "a": 1
+  }`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name: "small objects values not equal",
+			a:    `{"a": 1}`,
+			b:    `{"a": "1"}`,
+			wantDiff: `  {
+~   "a": "1" => 1
+  }`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "small objects keys not equal",
+			a:    `{"a": 1}`,
+			b:    `{"b": 1}`,
+			wantDiff: `  {
++   "a": 1,
+-   "b": 1
+  }`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "small objects extra elements",
+			a:    `{"a": 1}`,
+			b:    `{}`,
+			wantDiff: `  {
++   "a": 1
+  }`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "small objects missing elements",
+			a:    `{}`,
+			b:    `{"b": 1}`,
+			wantDiff: `  {
+-   "b": 1
+  }`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "large objects equal unordered",
+			a:    `{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}`,
+			b:    `{"c": 3, "b": 2, "a": 1, "e": 5, "d": 4}`,
+			wantDiff: `  {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5
+  }`,
+			wantKind: jsondiff.Match,
+		},
+
+		// Arrays
+		{
+			name:     "empty arrays equal",
+			a:        `[]`,
+			b:        `[]`,
+			wantDiff: `[]`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name: "small arrays equal",
+			a:    `["a"]`,
+			b:    `["a"]`,
+			wantDiff: `  [
+    "a"
+  ]`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name: "small arrays not equal",
+			a:    `["a"]`,
+			b:    `["b"]`,
+			wantDiff: `  [
+~   "b" => "a"
+  ]`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "small arrays missing element",
+			a:    `[]`,
+			b:    `["b"]`,
+			wantDiff: `  [
+-   "b"
+  ]`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "small arrays extra element",
+			a:    `["a"]`,
+			b:    `[]`,
+			wantDiff: `  [
++   "a"
+  ]`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "arrays respect order",
+			a:    `["a", "b"]`,
+			b:    `["b", "a"]`,
+			wantDiff: `  [
+~   "b" => "a",
+~   "a" => "b"
+  ]`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "large arrays equal",
+			a:    `["a", "b", "c", "d", "e"]`,
+			b:    `["a", "b", "c", "d", "e"]`,
+			wantDiff: `  [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e"
+  ]`,
+			wantKind: jsondiff.Match,
+		},
+
+		// Compound structures
+		{
+			name:     "empty object not equal empty array",
+			a:        `{}`,
+			b:        `[]`,
+			wantDiff: `[] => {}`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name:     "object not equal array",
+			a:        `{"a": 1}`,
+			b:        `["a", 1]`,
+			wantDiff: `[...] => {...}`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "objects containing array equal",
+			a:    `{"a": [1, 2, 3]}`,
+			b:    `{"a": [1, 2, 3]}`,
+			wantDiff: `  {
+    "a": [
+      1,
+      2,
+      3
+    ]
+  }`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name: "objects containing array not equal",
+			a:    `{"a": [1, 3, 2]}`,
+			b:    `{"a": [1, 2, 3]}`,
+			wantDiff: `  {
+    "a": [
+      1,
+~     2 => 3,
+~     3 => 2
+    ]
+  }`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "object containing empty array",
+			a:    `{"a": []}`,
+			b:    `{}`,
+			wantDiff: `  {
++   "a": []
+  }`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "object missing empty array",
+			a:    `{}`,
+			b:    `{"a": []}`,
+			wantDiff: `  {
+-   "a": []
+  }`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "object containing extra array",
+			a:    `{"a": [1, 2, 3]}`,
+			b:    `{}`,
+			wantDiff: `  {
++   "a": [
++     1,
++     2,
++     3
++   ]
+  }`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "object missing extra array",
+			a:    `{}`,
+			b:    `{"a": [1, 2, 3]}`,
+			wantDiff: `  {
+-   "a": [
+-     1,
+-     2,
+-     3
+-   ]
+  }`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "array containing object containing array equal",
+			a:    `[{"a": [1, 2, 3]}]`,
+			b:    `[{"a": [1, 2, 3]}]`,
+			wantDiff: `  [
+    {
+      "a": [
+        1,
+        2,
+        3
+      ]
+    }
+  ]`,
+			wantKind: jsondiff.Match,
+		},
+		{
+			name: "array containing object containing array not equal",
+			a:    `[{"a": [1, 2, 3]}]`,
+			b:    `[{"a": [1, 3, 2]}]`,
+			wantDiff: `  [
+    {
+      "a": [
+        1,
+~       3 => 2,
+~       2 => 3
+      ]
+    }
+  ]`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "array containing empty object",
+			a:    `[0, {}]`,
+			b:    `[0]`,
+			wantDiff: `  [
+    0,
++   {}
+  ]`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "array missing empty object",
+			a:    `[0]`,
+			b:    `[0, {}]`,
+			wantDiff: `  [
+    0,
+-   {}
+  ]`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "array containing extra object",
+			a:    `[0, {"a": 1}]`,
+			b:    `[0]`,
+			wantDiff: `  [
+    0,
++   {
++     "a": 1
++   }
+  ]`,
+			wantKind: jsondiff.NoMatch,
+		},
+		{
+			name: "array missing extra object",
+			a:    `[0]`,
+			b:    `[0, {"a": 1}]`,
+			wantDiff: `  [
+    0,
+-   {
+-     "a": 1
+-   }
+  ]`,
+			wantKind: jsondiff.NoMatch,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := ghost.New(t)
+
+			diff, kind := jsondiff.Diff(tt.a, tt.b)
+			g.Should(be.Equal(kind, tt.wantKind))
+			g.Should(be.Equal(diff, tt.wantDiff))
+		})
+	}
+}
+
+func TestKind_String(t *testing.T) {
+	g := ghost.New(t)
+
+	k := jsondiff.Kind(-1)
+	g.Should(be.Equal(k.String(), "InvalidKind"))
+}
